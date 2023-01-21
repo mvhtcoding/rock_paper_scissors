e4ece39f0b0cedb1831bb24a8c8a412c1b9c32ee
Matthew <110283696+mvhtcoding@users.noreply.github.com>
2023-01-21 21:16:49 +0100

    Finished end game code

--- script/domjavascript.js
+++ script/domjavascript.js
@@ -2,21 +2,20 @@ let computerPoints = 0;
 let playerPoints = 0;
 let playerSelection;
 let computerSelection;
-let roundWinner = document.querySelector('.roundWinner')
-let playerScore = document.querySelector('.playerScore')
-let computerScore = document.querySelector('.computerScore')
-let playerImage = document.querySelector('.playerImage')
-let computerImage = document.querySelector('.computerImage')
+let roundWinner = document.querySelector('.roundWinner');
+let playerScore = document.querySelector('.playerScore');
+let computerScore = document.querySelector('.computerScore');
+let playerImage = document.querySelector('.playerImage');
+let computerImage = document.querySelector('.computerImage');
+let buttons = document.querySelectorAll('button.btn');
 
-
-const buttons = document.querySelectorAll('button.btn');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerSelection = button.id;
-        playerImage.innerHTML = `<img src='img/${playerSelection}.png' alt='${playerSelection} image'/>`
+        playerImage.innerHTML = `<img src='img/${playerSelection}.png' alt='${playerSelection} image'/>`;
         computerSelection = getComputerChoice();
-        computerImage.innerHTML = `<img src='img/${computerSelection}.png' alt='${computerSelection} image'/>`
-        game();
+        computerImage.innerHTML = `<img src='img/${computerSelection}.png' alt='${computerSelection} image'/>`;
+        playRound(playerSelection, computerSelection);
     });
 });
 
@@ -37,8 +36,6 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
-
     if (playerSelection === computerSelection) {
         roundWinner.textContent = 'It\'s a tie';
     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
@@ -47,26 +44,18 @@ function playRound(playerSelection, computerSelection) {
         playerPoints++;
         roundWinner.textContent = 'You win!';
         playerScore.textContent = `Player: ${playerPoints}`
-
+        if (playerPoints === 5) {
+            alert('Congratulations! U win the best of 5, care for a rematch?');
+            window.location.reload();
+        }
     } else {
         computerPoints++;
         roundWinner.textContent = 'Computer wins';
         computerScore.textContent = `Computer: ${computerPoints}`;
-
+        if (computerPoints === 5) {
+            alert('Game over! The computer wins, better luck next time!');
+            window.location.reload();
+        }
     }
-
-
 }
 
-function game() {
-
-    if (playerPoints === 5 || computerPoints === 5) {
-        alert('Game over')
-        window.location.reload();
-
-    } else {
-
-    playRound(playerSelection, computerSelection)
-    }
-    
-}
